54258e62f3f60fa39f2abf5ce0f9a75286ea3c58
Henry Ewurum <hcewurum@gmail.com>
2021-01-14 00:35:30 -0500

    Started creating my template for courses.

---
+++ src/app/courses.components.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+
+@Component({
+    selector: 'courses', // we are referencing an element called courses
+    template: '<h2>Courses</h2>'
+})
+export  class CoursesComponent {
+
+}
