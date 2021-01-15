fb0ca5050228878e145bbd7937c4978276c66201
Henry Ewurum <hcewurum@gmail.com>
2021-01-14 23:40:50 -0500

    Created a new component that will be served from a different service. Also added the service to the list of providers.

--- src/app/courses.components.ts
+++ src/app/courses.components.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { CoursesService } from './course/courses.service';
 
 @Component({
     selector: 'courses', // we are referencing an element called courses
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+            {{ course }}
+            </li>
+        </ul>
+        
+    `
 })
 export  class CoursesComponent {
+    title = "List of courses";
+    courses;
+
+    constructor(service: CoursesService){
+        this.courses = service.getCourses();
+    }
+
+    //We usually get data from the server. So we need an HTTP service to do this
 
 }
